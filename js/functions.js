19e874911875e35b5868d855ce1666e11299299a
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-05-06 23:24:54 +0300

    Enable submenus toggle support in Primary menu.

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@
  * Enable tab support for dropdown menus.
  */
 ( function() {
-	var container, containerTop, menu, menuTop, links, linksTop;
+	var container, containerTop, menu, menuTop, links, linksTop, dropdownButton;
 
 	container = document.getElementById( 'menu-primary' );
 	
@@ -18,13 +18,72 @@
 		menu = container.getElementsByTagName( 'ul' )[0];
 
 		// Get all the link elements within the menu.
-		links = menu.getElementsByTagName( 'a' );
+		links    = menu.getElementsByTagName( 'a' );
+		subMenus = menu.getElementsByTagName( 'ul' );
 
 		// Each time a menu link is focused or blurred call the function toggleFocus.
 		for ( var i = 0, len = links.length; i < len; i++ ) {
 			links[i].onfocus = toggleFocus;
 			links[i].onblur = toggleFocus;
 		}
+		
+		// Set menu items with submenus to aria-haspopup="true".
+		for ( var i = 0, len = subMenus.length; i < len; i++ ) {
+			subMenus[i].parentNode.setAttribute( 'aria-haspopup', 'true' );
+		}
+		
+		// Add button after link when there is submenu around.
+		var parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+		
+		for ( var i = 0; i < parentLink.length; ++i ) {
+			parentLink[i].insertAdjacentHTML( 'afterend', '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
+		}
+		
+		// Select all dropdown buttons
+		dropdownButton = container.querySelectorAll( '.dropdown-toggle' );
+			
+		// For each dropdown Button element add click event
+		[].forEach.call( dropdownButton, function( el ) {
+
+			// Add click event listener
+			el.addEventListener( "click", function( event ) {
+				
+				// Change dropdown button text on every click
+				if( this.innerHTML === screenReaderText.expand ) {
+					this.innerHTML = screenReaderText.collapse;
+				} else {
+					this.innerHTML = screenReaderText.expand;
+				}
+				
+				// Toggle dropdown button
+				if( !hasClass( this, 'toggled' ) ) {
+					
+					// Add .toggled class
+					addClass( this, 'toggled' );
+					
+					// Set aria-expanded to true
+					this.setAttribute( 'aria-expanded', 'true' );
+					
+					// Get next element meaning UL with .sub-menu and add .toggled class
+					var nextElement = this.nextElementSibling;
+					addClass( nextElement, 'toggled' );
+					
+				} else {
+					
+					// Remove .toggled class
+					removeClass( this, 'toggled' );
+					
+					// Set aria-expanded to false
+					this.setAttribute( 'aria-expanded', 'false' );
+					
+					// Get next element meaning UL with .sub-menu and add .toggled class
+					var nextElement = this.nextElementSibling;
+					removeClass( nextElement, 'toggled' );
+					
+				}
+			}, false );
+
+		});
 		
 	}
 	
@@ -97,6 +156,47 @@
 	if ( container ) {
 		fixMenuTouchTaps( container );
 	}
+	
+	/**
+	* Adds a class to any element
+	*
+	* @param {element} element
+	* @param {string}  class
+	*/
+	addClass = function (el, cls) {
+		if (el.className.indexOf(cls) !== 0) {
+			el.className += " " + cls;
+			el.className = el.className.replace(/(^\s*)|(\s*$)/g,"");
+		}
+	}
+    
+	/**
+	* Remove a class from any element
+	*
+	* @param  {element} element
+	* @param  {string}  class
+	*/
+	removeClass = function (el, cls) {
+		var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+		el.className = el.className.replace(reg, " ").replace(/(^\s*)|(\s*$)/g,"");
+	}
+	
+	hasClass = function (elem, className) {
+		return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+	}
+	
+	/**
+	* forEach method that passes back the stuff we need
+	*
+	* @param  {array}    array
+	* @param  {Function} callback
+	* @param  {scope}    scope
+	*/
+	forEach = function (array, callback, scope) {
+		for (var i = 0; i < array.length; i++) {
+			callback.call(scope, i, array[i]);
+		}
+	};
 	
 } )();
 
